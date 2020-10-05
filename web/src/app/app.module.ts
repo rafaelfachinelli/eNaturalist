264e6fb9f8a725dda9fb0cf66f34f58e3133aa08
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-10-05 17:07:59 -0300

    nova pagina oqueeelixo

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { EcopointComponent } from './components/ecopoint/ecopoint.component';
+import { OqueEElixoComponent } from './pages/oque-e-elixo/oque-e-elixo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
-    EcopointComponent
+    EcopointComponent,
+    OqueEElixoComponent
   ],
   imports: [
     BrowserModule,
---
+++ web/src/app/pages/oque-e-elixo/oque-e-elixo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-oque-e-elixo',
+  templateUrl: './oque-e-elixo.component.html',
+  styleUrls: ['./oque-e-elixo.component.css']
+})
+export class OqueEElixoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
