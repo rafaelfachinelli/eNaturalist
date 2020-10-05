0333a9ef0f2d26960710dcf8c2e6b79771b5115e
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-10-05 15:45:37 -0300

    componentes adicionados na nova pasta

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { EcopointComponent } from './ecopoint/ecopoint.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { EcopointComponent } from './components/ecopoint/ecopoint.component';
 
 @NgModule({
   declarations: [
