bc9ee0f1da449f65fa9c9aff2382756619e6b3b1
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2020-11-05 16:36:42 -0300

    new project structure + ts + navigation + bt tabs

---
+++ mobile/src/screens/SearchEcopoints.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SearchEcopoints() {
+  return (
+    <View>
+      <Text>Essa aqui será a tela de busca dos ecopontos.</Text>
+    </View>
+  );
+}
---
+++ mobile/src/screens/Statistics.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function Statistics() {
+  return (
+    <View>
+      <Text>Visualizar Estatísticas</Text>
+    </View>
+  );
+}
